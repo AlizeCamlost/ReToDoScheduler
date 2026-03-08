627b4f709a1ac8f8d4ed26e666911f2c0c275d21
AlizeCamlost <camlost4036@gmail.com>
2026-03-08 14:33:17 +0800

    0308 deploy with web-app

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://43.159.136.45:8787";
+export const API_BASE_URL: string = import.meta.env.VITE_API_BASE_URL ?? "";
 
-export const API_AUTH_TOKEN = import.meta.env.VITE_API_AUTH_TOKEN ?? "";
+export const API_AUTH_TOKEN: string = import.meta.env.VITE_API_AUTH_TOKEN ?? "";
